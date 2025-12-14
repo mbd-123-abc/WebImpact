a1269b47673295755c218b67a6436fa6ac1db95e
mbd-123-abc <mahika13.3@gmail.com>
2025-12-15 05:02:00 +0530

    Update page.js

--- Portfolio/app/page.js
+++ Portfolio/app/page.js
@@ -1,5 +1,5 @@
 /*Mahika Bagri*/
-/*December 12 2025*/
+/*December 14 2025*/
 
 import Link from "next/link";
 import Image from "next/image";
@@ -7,13 +7,22 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <Link href="/cafe">
-        <img
-          src="/PortfolioEntry.png"
-          alt="Portfolio Entrance"
-          className="w-full h-full object-contain"
-        />
-      </Link>
+     <img
+        src="/PortfolioEntry.png"
+        alt="Portfolio Entrance"
+        className="w-full h-full object-contain"
+        priority
+      />
+      <Link href="/cafe"  
+      className="absolute z-10"
+      style={{
+        top: "45%",
+        left: "45%",
+        width: "8%",
+        height: "7%",
+      }}
+    />
+
     </div>
   );
 }
