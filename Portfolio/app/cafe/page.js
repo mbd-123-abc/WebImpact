b12f8ebccdc2c1a7fe9cddc77dc2bd56b9ff48a1
mbd-123-abc <mahika13.3@gmail.com>
2025-12-15 12:19:34 +0530

    Update page.js

--- Portfolio/app/cafe/page.js
+++ Portfolio/app/cafe/page.js
@@ -1,15 +1,14 @@
 /*Mahika Bagri*/
 /*December 15 2025*/
 
-
 export default function Cafe() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <Link href="/cafe">
-        <img
-          src="/public/Cafe.png"
-        />
-      </Link>
+      <img
+        src="/Cafe.png"
+        alt="Cafe Projects"
+        className="w-full h-full object-contain"
+      />
     </div>
   );
 }
